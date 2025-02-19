7cf239abda04a3d1b839be8923fdbf59d78c7966
Dvis <gokhaledurgesh4@gmail.com>
2025-02-19 14:50:28 +0530

    Update __main__.py

--- DURGESH/__main__.py
+++ DURGESH/__main__.py
@@ -1,6 +1,6 @@
 from DURGESH import LOGGER, app 
 
 if __name__ == "__main__":
-    LOGGER.info("❖ ᴛʜᴇ ᴀᴠɪsʜᴀ ᴄʜᴀᴛ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ.")
+    LOGGER.info("❖ ᴛʜᴇ ᴅᴜʀɢᴇsʜ ᴀɪ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ.")
     app().run()
   
